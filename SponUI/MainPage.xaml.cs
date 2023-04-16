796c1367d405fafd52008ac98405ecb797551d70
gianlouiemolinary <gianlouier97@gmail.com>
2023-04-16 18:42:45 -0400

    Add refresh view

--- SponUI/MainPage.xaml.cs
+++ SponUI/MainPage.xaml.cs
@@ -11,25 +11,10 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
 
-        LoadEvents(ref vm);
-
         BindingContext = vm;
+
+        vm.GetEvents();
     }
-
-    private static void LoadEvents(ref MainViewModel vm)
-    {
-        Random r = new Random();
-
-        for (int i = 0; i < 3; i++)
-        {
-            List<Attendant> attendants = new List<Attendant>() { new Attendant(), new Attendant(), new Attendant() };
-
-            vm.Events.Add(new Event() { Photo = new Image() { Source="bowling.png" }, Activity="Bowling", Place="Boardwalk Bowl", Price = r.Next(1, 20), Attendants = attendants });
-            vm.Events.Add(new Event() { Photo = new Image() { Source = "hiking_trail.png" }, Activity="Hiking", Place="Nature Trail", Attendants = attendants});
-        }
-    }
-
-
 }
 
 
--- SponUI/ViewModel/MainViewModel.cs
+++ SponUI/ViewModel/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using SponUI.Enums;
 using SponUI.Models;
 
@@ -19,6 +20,25 @@ namespace SponUI.ViewModel
 
         [ObservableProperty]
         Event ev;
+
+        [ObservableProperty]
+        bool isRefreshing;
+
+        [RelayCommand]
+        public void GetEvents()
+        {
+            //Events.Clear();
+
+            Random r = new Random();
+           
+            List<Attendant> attendants = new List<Attendant>() { new Attendant(), new Attendant(), new Attendant() };
+
+            Events.Add(new Event() { Photo = new Image() { Source = "bowling.png" }, Activity = "Bowling", Place = "Boardwalk Bowl", Price = r.Next(1, 20), Attendants = attendants });
+            //Events.Add(new Event() { Activity = "Hiking", Place = "Nature Trail", Attendants = attendants });
+            
+
+            IsRefreshing = false;
+        }
     }
 }
 
